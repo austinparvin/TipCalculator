465d11bd6ceb489569a31d4f3d923da753db4c62
Austin Parvin <austin.parvin@gmail.com>
2020-02-10 14:38:41 -0500

    Created first version

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TipCalculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // welcome the User
+      Console.WriteLine("Welcome User!!!");
+      // ask how much the bill 
+      Console.WriteLine("How much was your meal?");
+      var total = Console.ReadLine();
+      // calculate 18%
+      var tip = Double.Parse(total) * 0.18;
+      //Convert.ToDouble will accept any type Double.Parse only takes a string
+      // add tip to the total
+      var grandTotal = Double.Parse(total) + tip;
+      // display the result
+      Console.WriteLine("Your total was " + total);
+      Console.WriteLine("The 18% tip is " + total);
+      Console.WriteLine("Grandtotal is " + grandTotal);
+    }
+  }
+}
